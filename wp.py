c4105913afb7998b92955aef10a552730dfacb1c
Max <maxruigrok190@gmail.com>
2024-04-17 11:35:39 +0200

    shortcut parsing works now

--- wp.py
+++ wp.py
@@ -8,11 +8,17 @@ class Flag:
         self.f_name = f_name
         self.f_type = f_type
         self.f_comment = f_comment
+
+class ShortcutFlag:
+    def __init__(self, s_name:str, flag: Flag, data):
+        self.s_name = s_name
+        self.flag = (flag, data)
     
 
 class WP:
 
-    flags = []
+    flags: Flag = []
+    shortcuts = []
 
 
     def parse_flag(self, flag_string):
@@ -20,7 +26,7 @@ class WP:
         if len(flag_list) != 3:
             raise Exception("Flag in unsupported format.")
         
-        f_name = flag_list[0][1:]
+        f_name = flag_list[0]
         if (flag_list[1] in ["int", "integer"]):
             f_type = int
         elif (flag_list[1] in ["str", "string"]):
@@ -32,6 +38,24 @@ class WP:
         
         self.flags.append(Flag(f_name, f_type, flag_list[2]))
 
+    def parse_shortcut(self, shortcut_string):
+        shortcut_list = shlex.split(shortcut_string)
+        flag_list: ShortcutFlag = []
+        s_name = shortcut_list[0]
+        for i in range(1, len(shortcut_list), 2):
+            for j in range(len(self.flags)):
+                if shortcut_list[i] == self.flags[j].f_name:
+                    if self.flags[j].f_type ==  int:
+                        data = int(shortcut_list[i + 1])
+                    elif self.flags[j].f_type == float:
+                        data = float(shortcut_list[i + 1])
+                    else:
+                        data = shortcut_list[i + 1]
+                    
+                    flag_list.append(ShortcutFlag(self.flags[j].f_name, self.flags[j], data))
+        
+        self.shortcuts.append((shortcut_list[0], flag_list))
+
 
     def __init__(self, config_file):
         for line in config_file.readlines():
@@ -40,17 +64,12 @@ class WP:
             elif line.startswith('-'):
                 self.parse_flag(line)
             elif line.startswith('='):
-                self.parse_shortcut()
+                self.parse_shortcut(line)
             elif line in ["", "\n"]:
                 pass
             else:
                 raise Exception("Unexpected token encountered.")
             
-
-
-
-
-
 
 def main():
     if "wconfig" not in listdir():
@@ -59,7 +78,13 @@ def main():
     wp = WP(open("wconfig", 'r'))
 
     for flag in wp.flags:
-        print("-" + flag.f_name + " <" + flag.f_type.__name__ + "> " + flag.f_comment)
+        print(flag.f_name + " <" + flag.f_type.__name__ + "> ->" + flag.f_comment)
+
+    for sc in wp.shortcuts:
+        print(sc[0], end=" ")
+        for flag in sc[1]:
+            print(flag.s_name + " " + str(flag.flag[1]), end=" ")
+        print("\n")
     
 
 if __name__ == "__main__":
